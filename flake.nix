74a53a4865f3f14fc6a234ce71dc505568cd9860
Joseph DALY <joseph300905@gmail.com>
2024-07-10 12:26:21 +0200

    github actions

--- flake.nix
+++ flake.nix
@@ -1,15 +1,20 @@
 {
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs";
+    nix-github-actions.url = "github:nix-community/nix-github-actions";
+    nix-github-actions.inputs.nixpkgs.follows = "nixpkgs";
+  };
 
-  outputs = { nixpkgs, ... }: let
+  outputs = { self, nixpkgs, nix-github-actions, ... }: let
     eachSystem = fn: nixpkgs.lib.genAttrs [
       "x86_64-linux"
-      "aarch64-linux"
+      "x86_64-darwin"
     ] (system: (fn {
       inherit system;
       pkgs = (import nixpkgs { inherit system; } );
     }));
   in {
+    githubActions = nix-github-actions.lib.mkGithubMatrix { checks = self.packages; };
     devShells = eachSystem ({ pkgs, ... }: { default = pkgs.callPackage ./shell.nix {}; });
     packages = eachSystem ({ pkgs, ... }: { default = pkgs.callPackage ./default.nix {}; });
   };
