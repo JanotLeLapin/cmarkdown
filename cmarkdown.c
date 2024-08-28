4db5cb6a466439b30824fc03333d102826017cfd
Joseph DALY <joseph300905@gmail.com>
2024-08-28 13:56:04 +0200

    basic markdown data structures

--- cmarkdown.c
+++ cmarkdown.c
@@ -1,5 +1,90 @@
+#include "cmarkdown.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+struct CMarkNode
+create_node(enum CMarkNodeType type, union CMarkNodeData data, size_t initial_size)
+{
+  struct CMarkNode node, *children;
+  children = initial_size ? malloc(sizeof(struct CMarkNode) * initial_size) : NULL;
+  node.type = type;
+  node.data = data;
+  node.children_count = 0;
+  node.children_size = initial_size;
+  node.children = children;
+
+  return node;
+}
+
+void
+add_child(struct CMarkNode *node, struct CMarkNode child)
+{
+  if (!node->children) {
+    node->children = malloc(sizeof(struct CMarkNode));
+    node->children_size = 1;
+  }
+
+  if (node->children_count >= node->children_size) {
+    node->children = realloc(node->children, sizeof(struct CMarkNode) * node->children_size * 2);
+  }
+
+  node->children[node->children_count] = child;
+  node->children_count++;
+}
+
+void
+free_node(struct CMarkNode node)
+{
+  size_t i;
+
+  if (CMARK_PLAIN == node.type) {
+    free(node.data.plain);
+  }
+
+  for (i = 0; i < node.children_count; i++) {
+    free_node(node.children[i]);
+  }
+  free(node.children);
+}
+
 int
 main()
 {
+  union CMarkNodeData ad, bd, cd;
+  struct CMarkNode a, b, c;
+  char *at, *bt;
+  size_t i;
+
+  at = malloc(32);
+  bt = malloc(32);
+  strcpy(at, "hello, world!");
+  strcpy(bt, "foo bar");
+
+  ad.plain = at;
+  bd.plain = bt;
+  cd.header.level = 2;
+
+  a = create_node(CMARK_PLAIN, ad, 0);
+  b = create_node(CMARK_PLAIN, bd, 5);
+  c = create_node(CMARK_HEADER, cd, 2);
+
+  add_child(&c, a);
+  add_child(&c, b);
+
+  for (i = 0; i < c.children_count; i++) {
+    switch (c.children[i].type) {
+      case CMARK_PLAIN:
+        printf("found plain text: %s\n", c.children[i].data.plain);
+        break;
+      default:
+        printf("found other\n");
+        break;
+    }
+  }
+
+  free_node(c);
+
   return 0;
 }
---
+++ cmarkdown.h
@@ -0,0 +1,28 @@
+typedef typeof(sizeof(0)) size_t;
+
+enum CMarkNodeType {
+  CMARK_HEADER,
+
+  CMARK_PLAIN,
+};
+
+struct CMarkNodeHeaderData {
+  unsigned char level;
+};
+
+union CMarkNodeData {
+  struct CMarkNodeHeaderData header;
+  char *plain;
+};
+
+struct CMarkNode {
+  enum CMarkNodeType type;
+  union CMarkNodeData data;
+  size_t children_count;
+  size_t children_size;
+  struct CMarkNode *children;
+};
+
+struct CMarkNode create_node(enum CMarkNodeType type, union CMarkNodeData data, size_t initial_size);
+void add_child(struct CMarkNode *node, struct CMarkNode child);
+void free_node(struct CMarkNode node);
