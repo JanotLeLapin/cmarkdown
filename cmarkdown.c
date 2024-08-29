41e5f324ae47be7690a1211617ee4ca8e55b36e7
Joseph DALY <joseph300905@gmail.com>
2024-08-29 17:59:51 +0200

    basic parsing

--- cmarkdown.c
+++ cmarkdown.c
@@ -49,42 +49,91 @@ free_node(struct CMarkNode node)
   free(node.children);
 }
 
+struct CMarkContext *
+create_context(void *file)
+{
+  struct CMarkContext *ctx = malloc(sizeof(struct CMarkContext));
+  ctx->file = file;
+  return ctx;
+}
+
+void
+read_line(struct CMarkContext *ctx)
+{
+  fgets(ctx->buffer, sizeof(ctx->buffer), ctx->file);
+  ctx->i = 0;
+}
+
+struct CMarkNode
+parse_inline(struct CMarkContext *ctx)
+{
+  union CMarkNodeData data;
+  char *ptr = ctx->buffer + ctx->i;
+  size_t len;
+
+  len = strlen(ptr);
+  data.plain = malloc(len);
+  strncpy(data.plain, ptr, len - 1);
+
+  return create_node(CMARK_PLAIN, data, 0);
+}
+
+struct CMarkNode
+parse_header(struct CMarkContext *ctx)
+{
+  union CMarkNodeData data;
+  struct CMarkNode node, child;
+  size_t start = ctx->i;
+ 
+  while ('#' == ctx->buffer[ctx->i]) {
+    ctx->i++;
+  }
+  data.header.level = (unsigned char) (ctx->i - start);
+
+  child = parse_inline(ctx);
+
+  node = create_node(CMARK_HEADER, data, 1);
+  add_child(&node, child);
+  return node;
+}
+
+struct CMarkNode
+parse(struct CMarkContext *ctx)
+{
+  struct CMarkNode root = create_node(CMARK_ROOT, (union CMarkNodeData) { .root = 0 }, 8);
+
+  read_line(ctx);
+  add_child(&root, parse_header(ctx));
+
+  return root;
+}
+
 int
 main()
 {
-  union CMarkNodeData ad, bd, cd;
-  struct CMarkNode a, b, c;
-  char *at, *bt;
+  FILE *file;
+  struct CMarkContext *ctx;
+  struct CMarkNode root, node;
   size_t i;
 
-  at = malloc(32);
-  bt = malloc(32);
-  strcpy(at, "hello, world!");
-  strcpy(bt, "foo bar");
+  file = fopen("file.md", "r");
+  ctx = create_context(file);
+  root = parse(ctx);
 
-  ad.plain = at;
-  bd.plain = bt;
-  cd.header.level = 2;
-
-  a = create_node(CMARK_PLAIN, ad, 0);
-  b = create_node(CMARK_PLAIN, bd, 5);
-  c = create_node(CMARK_HEADER, cd, 2);
-
-  add_child(&c, a);
-  add_child(&c, b);
-
-  for (i = 0; i < c.children_count; i++) {
-    switch (c.children[i].type) {
-      case CMARK_PLAIN:
-        printf("found plain text: %s\n", c.children[i].data.plain);
+  for (i = 0; i < root.children_count; i++) {
+    node = root.children[i];
+    switch (node.type) {
+      case CMARK_HEADER:
+        printf("Found header of level %d with: '%s'\n", node.data.header.level, node.children[0].data.plain);
         break;
       default:
-        printf("found other\n");
         break;
     }
   }
 
-  free_node(c);
-
+  free(ctx);
+  fclose(file);
+  free_node(root);
+  
   return 0;
 }
--- cmarkdown.h
+++ cmarkdown.h
@@ -1,6 +1,8 @@
 typedef typeof(sizeof(0)) size_t;
 
 enum CMarkNodeType {
+  CMARK_ROOT,
+  
   CMARK_HEADER,
 
   CMARK_PLAIN,
@@ -13,6 +15,7 @@ struct CMarkNodeHeaderData {
 union CMarkNodeData {
   struct CMarkNodeHeaderData header;
   char *plain;
+  char root;
 };
 
 struct CMarkNode {
@@ -23,6 +26,15 @@ struct CMarkNode {
   struct CMarkNode *children;
 };
 
+struct CMarkContext {
+  void *file;
+  char buffer[512];
+  size_t i;
+};
+
 struct CMarkNode create_node(enum CMarkNodeType type, union CMarkNodeData data, size_t initial_size);
 void add_child(struct CMarkNode *node, struct CMarkNode child);
 void free_node(struct CMarkNode node);
+
+struct CMarkContext *create_context(void *file);
+struct CMarkNode parse(struct CMarkContext *ctx);
