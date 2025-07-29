925448512138d09fd69b14879829ac9b70da7977
Joseph DALY <joseph300905@gmail.com>
2025-07-29 21:48:49 +0200

    provide static cmarkdown library

--- default.nix
+++ default.nix
@@ -7,13 +7,15 @@
   src = ./.;
 
   buildPhase = ''
-    $CC -c cmarkdown.c -o cmarkdown.o
+    $CC -fPIC -c cmarkdown.c -o cmarkdown.o
     $CC -shared -o libcmarkdown.so cmarkdown.o
+    ar rcs libcmarkdown.a cmarkdown.o
   '';
   installPhase = ''
     mkdir -p $out/lib
     mkdir -p $out/include
     cp libcmarkdown.so $out/lib
+    cp libcmarkdown.a $out/lib
     cp cmarkdown.h $out/include
   '';
 }
